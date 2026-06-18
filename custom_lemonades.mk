3c204861433c5fa0e746f628c60152e389578797
neokoni <dulin081682@126.com>
2026-06-18 21:51:21 +0800

    lemonades: bring up for uwuAOSP
    Signed-off-by: neokoni <dulin081682@126.com>

--- custom_lemonades.mk
+++ custom_lemonades.mk
@@ -13,12 +13,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/oneplus/lemonades/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/custom/config/common_full_phone.mk)
 
-# Inherit some our staff
-$(call inherit-product, device/oneplus/lemonades/avium.mk)
-
-PRODUCT_NAME := lineage_lemonades
+PRODUCT_NAME := custom_lemonades
 PRODUCT_DEVICE := lemonades
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
